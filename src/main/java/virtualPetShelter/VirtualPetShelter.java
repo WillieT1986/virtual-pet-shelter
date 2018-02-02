66f746bb025149c72ec6b717515c90cafedd16c3
William <WarMad2@Hotmail.com>
2018-02-02 14:13:53 -0500

    Under VirtualPetShelterTest, ran test to find specific pet name - test passed

--- src/main/java/virtualPetShelter/VirtualPetShelter.java
+++ src/main/java/virtualPetShelter/VirtualPetShelter.java
@@ -1,5 +1,20 @@
 package virtualPetShelter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VirtualPetShelter {
+
+	private Map<String, VirtualPet> pets = new HashMap<>();
+
+	public VirtualPet findPet(String petName) {
+		return pets.get(petName);
+	}
+
+	// Return specific Virtual Pet given its name
+	public void add(VirtualPet virtualPet) {
+		String newName = virtualPet.getName();
+		pets.put(newName, virtualPet);
+	}
 
 }
--- src/test/java/virtualPetShelter/VirtualPetShelterTest.java
+++ src/test/java/virtualPetShelter/VirtualPetShelterTest.java
@@ -1,8 +1,16 @@
 package virtualPetShelter;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class VirtualPetShelterTest {
+	private static final String PET_NAME = "name";
+	private static final String DESCRIPTION = "description";
+	private VirtualPetShelter underTest;
+	// private static final String Pet_Description = "description";
 
 	// @Test
 	// public void shouldInstantiateVirtualPetShelter() {
@@ -11,11 +19,17 @@ public class VirtualPetShelterTest {
 	// assertNotNull(underTest);
 	// }
 
+	@Before
+	public void setup() {
+		underTest = new VirtualPetShelter();
+	}
+
 	@Test
 	public void shouldAddVirtualPetName() {
-		VirtualPetShelter underTest = new VirtualPetShelter();
-		underTest.add(VirtualPet(name));
-
+		VirtualPet check = new VirtualPet(PET_NAME, DESCRIPTION);
+		underTest.add(check);
+		VirtualPet retrieved = underTest.findPet(PET_NAME);
+		assertThat(retrieved, is(check));
 	}
 
 }
