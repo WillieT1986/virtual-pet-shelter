dafb25752a826d0b10853f8dea5f819ec4d224af
William <WarMad2@Hotmail.com>
2018-02-02 10:47:22 -0500

    Created VirtualPet Class through testing

--- src/test/java/virtualPetShelter/VirtualPetTest.java
+++ src/test/java/virtualPetShelter/VirtualPetTest.java
@@ -1,5 +1,16 @@
 package virtualPetShelter;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
 public class VirtualPetTest {
+
+	@Test
+	public void shouldInstantiateVirtualPet() {
+		VirtualPet underTest = new VirtualPet();
+
+		assertNotNull(underTest);
+	}
 
 }
