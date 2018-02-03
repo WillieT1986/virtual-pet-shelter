ff99b05cab973fd8b7ffe41acd84d4699502188b
William <WarMad2@Hotmail.com>
2018-02-03 15:35:12 -0500

    Added Scanner to VirtualPetShelterApp

--- src/main/java/virtualPetShelter/VirtualPetShelterApp.java
+++ src/main/java/virtualPetShelter/VirtualPetShelterApp.java
@@ -1,9 +1,13 @@
 package virtualPetShelter;
 
+import java.util.Scanner;
+
 public class VirtualPetShelterApp {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
+		input.close();
 	}
 
 }
