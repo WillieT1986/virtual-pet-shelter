91567e527873db01c0e083c6502f42e94a2849b3
William <WarMad2@Hotmail.com>
2018-02-03 23:49:39 -0500

    Added the main greeting under the VirtualPetShelterApp, Thank you for volunteering at Big Al's Virtual Pet Shelter and Delicatessen!

--- src/main/java/virtualPetShelter/VirtualPetShelterApp.java
+++ src/main/java/virtualPetShelter/VirtualPetShelterApp.java
@@ -7,6 +7,8 @@ public class VirtualPetShelterApp {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
+		System.out.println("Thank you for volunteering at Big Al's Virtual Pet Shelter and Delicatessen!");
+
 		input.close();
 	}
 
